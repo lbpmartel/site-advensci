5061044b697a407be25e51f4e645220d528a55fb
Louis-Bernard Poulin <lbpmartel@gmail.com>
2018-08-16 22:10:36 -0400

    jumbotron + debut mission

--- advensci/src/App.js
+++ advensci/src/App.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import Mission from './components/Mission';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <main>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Mission<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Notre équipe<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Prix<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contactez-nous<span class="sr-only">(current)</span></a>
+        </li>
+      </ul>
+      </div>
+    </nav>
+    <Header />
+    <Mission />
+    </main>
+
     );
   }
 }
---
+++ advensci/src/components/Mission.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class Mission extends React.Component {
+  render() {
+    return (
+      <div class="container-fluid mission">
+        <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <p>
+              Advensci est une plateforme accessible qui permet
+              aux chercheurs de recruter des participant.e.s pour
+              leurs études.
+            </p>
+          </div>
+          <div class="col-md-4">
+            <p>
+              Découvre qui se fait dans le monde de la recherche en
+              trouvant des études qui t'intéressent à partir d'une
+              application facile à utiliser.
+            </p>
+          </div>
+          <div class="col-md-4">
+            <p>
+              Bénéficie de compensations financières tout en
+              contribuant à l’avancement de la science!
+            </p>
+          </div>
+        </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Mission;

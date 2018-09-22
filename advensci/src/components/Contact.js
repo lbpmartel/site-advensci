81b3a64d40f90e5279d0f17035721d28e6a7ed90
Louis-Bernard Poulin <lbpmartel@gmail.com>
2018-09-22 11:06:24 -0400

    Added tab for forms

--- advensci/src/components/Contact.js
+++ advensci/src/components/Contact.js
@@ -6,11 +6,26 @@ class Contact extends React.Component {
     return (
       <div class="container-fluid">
         <div class="container">
-        <div class="row">
-          <div class="col-12 m-auto">
-              <ParticipantForm />
+          <div class="row">
+            <nav class="nav nav-tabs col-12">
+              <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item">
+                  <a class="nav-link active" id="participant-tab" data-toggle="tab" href="#participant" role="tab" aria-controls="participant" aria-selected="true">Participant</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" id="chercheur-tab" data-toggle="tab" href="#chercheur" role="tab" aria-controls="chercheur" aria-selected="false">Chercheur</a>
+                </li>
+              </ul>
+              <div class="tab-content col-12" id="myTabContent">
+                <div class="tab-pane fade show active" id="participant" role="tabpanel" aria-labelledby="participant-tab">
+                  <ParticipantForm />
+                </div>
+                <div class="tab-pane fade" id="chercheur" role="tabpanel" aria-labelledby="chercheur-tab">
+                  BLA BLA BLA
+                  </div>
+              </div>
+            </nav>
           </div>
-        </div>
         </div>
       </div>
     );

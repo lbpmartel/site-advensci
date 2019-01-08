463cf92d08dc7ec8a652f8637e85324a3ba8248c
niafreu <luisgaylornobre@gmail.com>
2019-01-08 09:25:12 -0500

    Menu resize, logo centered and bg

--- src/App.js
+++ src/App.js
@@ -14,25 +14,25 @@ class App extends Component {
     render() {
         return (
             <main>
-                <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-                    <div class="container">
-                        <a class="navbar-brand" href="#">
-                            <img class="logo" src="/img/logo.png" alt="" />
+                <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="navbar">
+                    <div className="container">
+                        <a className="navbar-brand" href="#" id="navbar-brand">
+                            <img className="logo" src="/img/logo.png" alt="" id="logo" />
                         </a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
                         </button>
 
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav ml-auto">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="mailto:info@advensci.ca"><img src="img/ma.jpg" alt=""></img></a>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav ml-auto">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="mailto:info@advensci.ca"><img src="img/ma.png" alt=""></img></a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="http://facebook.com"><img src="img/fa.jpg" alt=""></img></a>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="http://facebook.com"><img src="img/fa.png" alt=""></img></a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="http://twitter.com"><img src="img/tw.jpg" alt=""></img></a>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="http://twitter.com"><img src="img/tw.png" alt=""></img></a>
                                 </li>
                             </ul>
                         </div>
@@ -49,9 +49,24 @@ class App extends Component {
                 <Footer />
 
             </main>
-
         );
     }
+}
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size and add white bg
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("logo").style.height = "84px";
+    document.getElementById("navbar").style.backgroundColor = "white";
+    document.getElementById("navbar").style.height = "133px";
+  } else {
+    document.getElementById("logo").style.height = "100%";
+    document.getElementById("navbar").style.backgroundColor = "transparent";
+    document.getElementById("navbar").style.height = "100%";
+    document.getElementById("navbar-brand").style.padding = "35px 0px";
+  }
 }
 
 export default App;

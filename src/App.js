f8f3b86b5f05dc2dca3ea7860e870dd32216c1ea
niafreu <luisgaylornobre@gmail.com>
2019-01-09 09:27:00 -0500

    Header et fonts terminés

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
                 <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="navbar">
                     <div className="container">
                         <a className="navbar-brand" href="#" id="navbar-brand">
-                            <img className="logo" src="/img/logo.png" alt="" id="logo" />
+                            <img className="logo img-fluid" src="/img/logo.png" alt="" id="logo" />
                         </a>
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
@@ -32,7 +32,7 @@ class App extends Component {
                                     <a className="nav-link" href="http://facebook.com"><img src="img/fa.png" alt=""></img></a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="http://twitter.com"><img src="img/tw.png" alt=""></img></a>
+                                    <a className="nav-link" href="http://twitter.com"><img src="img/twi.png" alt=""></img></a>
                                 </li>
                             </ul>
                         </div>
@@ -61,11 +61,16 @@ function scrollFunction() {
     document.getElementById("logo").style.height = "84px";
     document.getElementById("navbar").style.backgroundColor = "white";
     document.getElementById("navbar").style.height = "133px";
+    document.getElementById("navbar-brand").style.margin = "0px 0px";
+    document.getElementById("navbar-brand").style.left = "calc(50% - 35px)";
+    document.getElementById("navbarSupportedContent").style.marginBottom = "auto";
   } else {
     document.getElementById("logo").style.height = "100%";
     document.getElementById("navbar").style.backgroundColor = "transparent";
-    document.getElementById("navbar").style.height = "100%";
-    document.getElementById("navbar-brand").style.padding = "35px 0px";
+    document.getElementById("navbar").style.height = "auto";
+    document.getElementById("navbar-brand").style.marginTop = "35px";
+    document.getElementById("navbar-brand").style.left = "calc(50% - 56px)";
+    document.getElementById("navbarSupportedContent").style.marginBottom = "0";
   }
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,13 @@ class Header extends React.Component {
             <div class="jumbotron jumbotron-fluid background-header">
                 <div class="container header">
                     <div class="row">
-                        <div class="col-lg-8 header-text">
+                        <div class="col-lg-6 header-text">
                             <div class="header-rectangle">
                                 <h3>Advensci</h3>
                                 <p>Une application réunissant les participants motivés et les chercheurs.</p>
                             </div>
                         </div>
-                        <div class="col-lg-4">
+                        <div class="header-photo col-lg-6 d-flex">
                             <img src="/img/phone.png" alt=""></img>
                         </div>
                     </div>

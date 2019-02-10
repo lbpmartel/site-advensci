13128a05122c1625f1a823d97fb0a2ef59583848
Louis-Bernard Poulin <lbpmartel@gmail.com>
2019-02-10 14:15:47 -0500

    participant form + css

--- src/components/Prize.js
+++ src/components/Prize.js
@@ -4,9 +4,11 @@ class Prize extends React.Component {
 
   render() {
     return (
-      <div className="prize">
-        <h3 className="prize-name text-center">{this.props.prizeTitle}</h3>
-        <img className="prize-img" src={"./img/" + this.props.prizeImg} alt=" " />
+      <div className="prize row">
+        <div className="col">
+          <h3>{this.props.prizeTitle}</h3>
+          <img src={"./img/" + this.props.prizeImg} alt=" " />
+        </div>
       </div>
     );
   }

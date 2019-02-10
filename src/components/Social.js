0d349b5bf7d2f339bd4c1b4d56585056e476bb87
niafreu <luisgaylornobre@gmail.com>
2019-02-10 14:39:02 -0500

    Embed last tweet

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,10 +4,10 @@ class Social extends Component {
     render() {
         return (
             <div>
-                <section id="social" className="container-fluid">
+                <section id="social" className="container">
                     <div class="row">
                         <div class="col-lg-6">
-                        <a class="twitter-timeline" href="https://twitter.com/TwitterDev?ref_src=twsrc%5Etfw">Tweets by TwitterDev</a>
+                            <a className="twitter-timeline" href="https://twitter.com/TwitterDev?ref_src=twsrc%5Etfw" data-tweet-limit="1">Tweets d'Advensci</a>
                         </div>
                         <hr />
                     </div>

f2033672a6b8d6afeb5b9f968851a984216bd0ca
Louis-Bernard Poulin <lbpmartel@gmail.com>
2019-02-10 14:47:47 -0500

    facebook timeline

--- src/components/Social.js
+++ src/components/Social.js
@@ -6,10 +6,13 @@ class Social extends Component {
             <div>
                 <section id="social" className="container-fluid">
                     <div class="row">
-                        <div class="col-lg-6">
-                        <a class="twitter-timeline" href="https://twitter.com/TwitterDev?ref_src=twsrc%5Etfw">Tweets by TwitterDev</a>
+                        <div className="col-lg-6">
+                            <a className="twitter-timeline" href="https://twitter.com/TwitterDev?ref_src=twsrc%5Etfw">Tweets by TwitterDev</a>
                         </div>
                         <hr />
+                        <div className="col-lg-6">
+                            <iframe className="facebook-timeline" src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fadvensci.inc&width=600&colorscheme=light&show_faces=true&border_color&stream=true&header=true&height=435" scrolling="yes" frameborder="0" allowtransparency="true"></iframe>
+                        </div>
                     </div>
                 </section >
             </div >

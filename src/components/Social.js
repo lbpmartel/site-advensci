97d1f1b622b69c2b1c24a05c73006214dd30496f
lg-460s <luisgaylornobre@gmail.com>
2019-06-15 14:35:38 -0400

    centered sponsors

--- src/components/Social.js
+++ src/components/Social.js
@@ -10,10 +10,8 @@ class Social extends Component {
                      <a className="twitter-timeline" href="https://twitter.com/advensci" data-tweet-limit="1">Tweets d'Advensci</a>
                   </div>
 
-                  <hr />
-
                   <div className="col-lg-6">
-                     <iframe title="facebook-timeline" className="facebook-timeline" src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fadvensci.inc&width=600&colorscheme=light&show_faces=true&border_color&stream=true&header=true&height=435" scrolling="yes" frameBorder="0" allowtransparency="true"></iframe>
+                     <iframe title="facebook-timeline" className="facebook-timeline" src="https://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fadvensci.inc&width=600&colorscheme=light&show_faces=true&border_color&stream=true&header=true&height=435" scrolling="yes" frameBorder="0" allowtransparency="true"></iframe>
                   </div>
                </div>
             </section >
